7465f772c78d41ad2de40ec0bd431da3e9568b29
INAL357 <inalmendonca2@gmail.com>
2026-08-31 12:19:27 +0400

    added config

--- src/main/java/com/app/auth/auth_app_backend/entities/RefreshToken.java
+++ src/main/java/com/app/auth/auth_app_backend/entities/RefreshToken.java
@@ -1,8 +1,24 @@
 package com.app.auth.auth_app_backend.entities;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+
+import java.time.Instant;
 
 @Entity
+@Table(name = "entity")
 public class RefreshToken {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @OneToOne
+    @JoinColumn(name = "id", referencedColumnName = "user_id")
+    private User user;
+
+    @Column(nullable = false)
+    private Instant expiryDate;
 }
